f9a26ab57ddf8f18fcb9bf76af6e5958438515ab
s932344 <s932344@gmail.com>
2017-01-31 00:40:02 -0600

    init version

---
+++ UnitTest/BAL/GitIntegrationBALTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest.BAL
+{
+    [TestClass]
+    public class GitIntegrationBALTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
