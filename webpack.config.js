56ee50bfbbbf57bacce7c06438217d85a103291b
Cathy Huang <cathyhaung.tw@gmail.com>
2017-02-07 01:29:36 -0800

    Add Webpack bundler

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+var webpack = require('webpack');  
+module.exports = {  
+    entry: [
+      'webpack/hot/only-dev-server',
+      "./app/js/app.js"
+    ],
+    output: {
+        path: __dirname + '/build',
+        filename: "bundle.js"
+    },
+    module: {
+        loaders: [
+            { test: /\.jsx?$/,
+              loader: 'babel-loader',
+              exclude: /node_modules/,
+              query:
+              {
+                presets:['es2015', 'react']
+              }
+            },
+            { test: /\.css$/, loader: "style!css" }
+        ]
+    },
+    plugins: [
+      new webpack.NoErrorsPlugin()
+    ]
+
+};
