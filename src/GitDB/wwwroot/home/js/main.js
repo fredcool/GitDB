ae5040d12dfd5b896ea38955c82474e0275c6321
s932344 <s932344@gmail.com>
2017-03-21 04:55:08 -0500

    - Allow cross origin ajax call

---
+++ src/GitDB/wwwroot/home/js/main.js
@@ -0,0 +1,19 @@
+﻿{
+    $(document).ready(function() {
+        console.log(webServiceUrl);
+        $.ajax({
+            url: webServiceUrl + 'User/UserLogin',
+            method: 'POST',
+            data: {
+                Username: 's932344@gmail.com',
+                Password: 'Password1'
+            },
+            beforeSend: function() {
+
+            },
+            success: function (data) {
+                console.log(data);
+            }
+        });
+    });
+}($);
