ae1f35a7fbc279d3fb75a7a5b665d0343d58cc0a
s932344 <s932344@gmail.com>
2017-03-21 00:42:25 -0500

    - Update DB connection string

--- src/BAL/UserBAL.cs
+++ src/BAL/UserBAL.cs
@@ -3,10 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BusinessObject.Request;
+using BusinessObject.Response;
 
 namespace BAL
 {
-    class UserBAL
+    public class UserBAL : IUserBAL
     {
+        public UserLoginResponse UserLogin(UserLoginRequest request)
+        {
+            throw new NotImplementedException();
+        }
+
+        public UserRegistrationResponse UserRegistration(UserRegistrationRequest request)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
