898569e287b1c9e26915f8bda04c5308561b13b5
s932344 <s932344@gmail.com>
2017-03-20 22:43:55 -0500

    Add DAL and Model layer of User login and registration flow

---
+++ src/Model/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class User
+    {
+        #region SQL Scripts
+        public string ScriptGetUserByUsername
+        {
+            get
+            {
+                return " select * from [User] where Username = @Username ";
+            }
+            set { }
+        }
+        #endregion
+
+        public int UserID { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Name { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime UpdateDate { get; set; }
+    }
+}
