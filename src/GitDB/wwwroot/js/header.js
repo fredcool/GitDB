cb0397cd311afee6d9231baa133000930c2f40b8
Cathy Huang <cathyhaung.tw@gmail.com>
2017-03-25 14:38:48 -0700

    Fix Header route

--- src/GitDB/wwwroot/js/header.js
+++ src/GitDB/wwwroot/js/header.js
@@ -2,15 +2,14 @@ import React from 'react';
 import { PageHeader } from 'react-bootstrap';
 //import { Link } from 'react-router-dom';
 import { Link, IndexLink } from 'react-router';
+import * as url from './baseUrl';
 
 export default class Header extends React.Component {
 
   render() {
-    const baseUrl = "/src/GitDB/wwwroot/";
-    
     return (
       <div className="container">
-        <IndexLink to={baseUrl} 
+        <IndexLink to={url.baseUrl} 
           activeClassName="active">
           <PageHeader>GitDB <small> @ITU</small></PageHeader>
         </IndexLink>
