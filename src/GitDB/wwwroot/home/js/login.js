f75e2d1f8ec638e1087a17e2783d22d3b40580fd
Neofredcool <s932344@gmail.com>
2017-03-24 23:56:18 -0500

    - splash screen ui update

--- src/GitDB/wwwroot/home/js/login.js
+++ src/GitDB/wwwroot/home/js/login.js
@@ -1,6 +1,5 @@
 // A $( document ).ready() block.
 $( document ).ready(function() {
-    console.log( "ready!" );
 	
 	$(".reset").click(function() {
 	$ ("#myEmail").val("");	
@@ -13,42 +12,54 @@ $( document ).ready(function() {
 	$("#myPassword1").val("");
 		
 	});
-$(".loginButton").click(function() {
-var email = $("#myEmail").val();
-var password = $("#myPassword").val();
+	$(".loginButton").click(function () {
+	    var email = $("#myEmail").val();
+	    var password = $("#myPassword").val();
 
-$.post( "http://34.208.160.108/WebService/User/UserLogin", { Username: email, Password: password })  //s932344@gmail.com     password1
-  .done(function( data, page ) {
-  
-    console.log( data.StatusCode);
-	if(data.StatusCode == "0"){
-	$("#loginSuccess").show(); 
-	window.location = "http://34.208.160.108/Console/index.html";
-	$("#loginFailed").hide();
-	}
-	else{
-	$("#loginSuccess").hide();
-	$("#loginFailed").show();
-	}
-  });
-  });
-  $(".registerinButton").click(function() {
-var name = $ ("#myname").val();
-var email = $("#myEmail1").val();
-var password = $("#myPassword1").val();
+	    $.post("http://34.208.160.108/WebService/User/UserLogin", { Username: email, Password: password })  //s932344@gmail.com     password1
+          .done(function (data, page) {
+              console.log(data.StatusCode);
+              if (data.StatusCode == "0") {
+                  $("#loginStatus").attr('class', 'alert');
+                  $("#loginStatus").addClass('alert-success');
+                  $("#loginStatus").html('Login successfully');
+                  window.location = "http://34.208.160.108/Console/index.html";
+              }
+              else {
+                  $("#loginStatus").attr('class', 'alert');
+                  $("#loginStatus").addClass('alert-danger');
+                  $("#loginStatus").html('Login failed. Please try again.');
+              }
+          }).error(function () {
+              $("#loginStatus").attr('class', 'alert');
+              $("#loginStatus").addClass('alert-danger');
+              $("#loginStatus").html('Login failed. Please try again.');
+          });
+	});
 
-$.post( "http://34.208.160.108/WebService/User/UserRegistration", { Name: name, Username: email, Password: password })  
-  .done(function( data ) {
-  
-    console.log( data.StatusCode);
-	if(data.StatusCode == "0"){ 
-	$("#registerinSuccess").show();
-	$("#registerinFailed").hide();
-	}
-	else{
-	$("#registerinSuccess").hide();
-	$("#registerinFailed").show();
-	}
-  });
-  });
+	$(".registerinButton").click(function() {
+	    var name = $ ("#myname").val();
+	    var email = $("#myEmail1").val();
+	    var password = $("#myPassword1").val();
+
+	    $.post( "http://34.208.160.108/WebService/User/UserRegistration", { Name: name, Username: email, Password: password })  
+            .done(function( data ) {
+              console.log( data.StatusCode);
+              if(data.StatusCode == "0"){ 
+                  $("#registrationStatus").attr('class', 'alert');
+                  $("#registrationStatus").addClass('alert-success');
+                  $("#registrationStatus").html('Register successfully');
+              }
+              else{
+                  $("#registrationStatus").attr('class', 'alert');
+                  $("#registrationStatus").addClass('alert-danger');
+                  $("#registrationStatus").html('Register failed');
+              }
+          })
+            .error(function () {
+                $("#registrationStatus").attr('class', 'alert');
+                $("#registrationStatus").addClass('alert-danger');
+                $("#registrationStatus").html('Register failed');
+          });
+      });
 });
